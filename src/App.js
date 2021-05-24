395b4711caeb6daaff579410f61cfdcefaf82eb9
Yusuf <yusufelyano@gmail.com>
2021-05-24 17:47:08 +0300

    calc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+   const [FirstNumber, setFirstNumber] = useState("");
+   const [SecondNumber, setSecondNumber] = useState("");
+   const [Result, setResult] = useState("")
+   const plus = () => {
+      setResult(parseInt(FirstNumber) + parseInt(SecondNumber) )
+   }
+   const minus = () => {
+      setResult(FirstNumber - SecondNumber)
+   }
+   const multiply = () => {
+      setResult(FirstNumber * SecondNumber)
+   }
+   const divide = () => {
+      setResult(FirstNumber / SecondNumber)
+   }
+
+   const handleFirst = (e) => {
+      setFirstNumber(e.target.value)
+   }
+   const handleTwo = (e) => {
+      setSecondNumber(e.target.value)
+   }
+   return(
+      <div className="app"> 
+      <div className="form">
+         <input
+         className="input"
+          type="text"
+          value={FirstNumber}
+          onChange={handleFirst}
+          placeholder="A"
+          />
+         <input
+          className="input"
+          type="text"
+          value={SecondNumber}
+          onChange={handleTwo}
+          placeholder="B"
+            />
+         </div>  
+         <div className="result__area">Результат: <div className="result">{Result}</div></div>
+         <div className="buttons">
+         <button onClick={plus} className="btn">+</button>
+         <button onClick={minus} className="btn">-</button>
+         <button onClick={multiply} className="btn">*</button>
+         <button onClick={divide} className="btn">/</button>
+         </div>
+      </div>
+      
+   )
 }
 
-export default App;
+export default App
